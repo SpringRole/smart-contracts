64523287ac62d5eab497fb4ec2dad2aecf6f6e7b
Kartik Mandaville <kar2905@users.noreply.github.com>
2018-01-12 14:13:53 +0530

    change variable name

--- Vanity.sol
+++ Vanity.sol
@@ -134,7 +134,7 @@ contract VanityURL is Ownable,Pausable {
     transferTokenTo = _transferTokenTo;
   }
 
-  event VanityReserved(address _from, string _vanity_url);
+  event VanityReserved(address _to, string _vanity_url);
 
   /* function to update Token address */
   function updateTokenAddress (address _tokenAddress) onlyOwner public {
