35bfb09c5e7d0a3b1bf3593d7ae40cb2beb726b4
Vinay Agarwal <vinay035@gmail.com>
2018-03-28 00:15:28 +0530

    added test cases

--- test/2_airdrop.js
+++ test/2_airdrop.js
@@ -5,6 +5,8 @@ contract('Airdrop', function(accounts) {
 
     var airdropInstance;
     var tokenInstance;
+    // random address as need 0 eth balance account to test
+    var zero_balance = '0xf59b7E0F657B0DCBaD411465F5A534121fF42B58';
 
     before(function () {
         return AirDrop.deployed().then(function(instance) {
@@ -38,16 +40,24 @@ contract('Airdrop', function(accounts) {
 
     it("do 1 token airdrop to 2 different address", function() {
         var amount = 1;
-        var address = [accounts[1],accounts[2]];
+        var eth_amount = 100;
 
         // Get initial balances of first and second account.
         var account_one = accounts[1];
-        var account_two = accounts[2];
+        var account_two = zero_balance;
 
         var account_one_starting_balance;
         var account_two_starting_balance;
         var account_one_ending_balance;
         var account_two_ending_balance;
+
+        var account_one_eth_starting_balance = web3.eth.getBalance(account_one).toNumber();
+        var account_two_eth_starting_balance = web3.eth.getBalance(account_two).toNumber();
+
+        var account_one_eth_ending_balance;
+        var account_two_eth_ending_balance;
+
+        var address = [account_one,account_two];
 
         return tokenInstance.mint(10000)
         .then(function(instance){
@@ -63,7 +73,7 @@ contract('Airdrop', function(accounts) {
         }).then(function(balance){
             account_two_starting_balance = balance.toNumber();
             // do airdrop
-            return airdropInstance.doAirDrop(address,amount,100);
+            return airdropInstance.doAirDrop(address,amount,eth_amount);
         }).then(function(){
             // check balance for account 1
             return tokenInstance.balanceOf.call(account_one);
@@ -76,6 +86,13 @@ contract('Airdrop', function(accounts) {
             // check if balance has updated or not
             assert.equal(account_one_ending_balance, account_one_starting_balance + amount, "Amount wasn't sent to the receiver one");
             assert.equal(account_two_ending_balance, account_two_starting_balance + amount, "Amount wasn't correctly sent to the receiver two");
+            // fetch final eth balance
+            account_one_eth_ending_balance = web3.eth.getBalance(account_one).toNumber();
+            account_two_eth_ending_balance = web3.eth.getBalance(account_two).toNumber();
+
+            // check if eth is updated for 2nd and remains the same for first
+            assert.equal(account_one_eth_ending_balance, account_one_eth_starting_balance, "Eth balance is not the same");
+            assert.equal(account_two_eth_ending_balance, account_two_eth_starting_balance + eth_amount, "Eth balance was not update correctly");
         });
     });
 });
