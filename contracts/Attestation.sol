a2d22419e0551c784460cb6c3e7136de3873dc46
Vinay Agarwal <vinay035@gmail.com>
2018-06-12 20:02:23 +0530

    added owner check

--- contracts/Attestation.sol
+++ contracts/Attestation.sol
@@ -1,6 +1,42 @@
 pragma solidity 0.4.19;
 
-contract Attestation {
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() {
+    owner = msg.sender;
+  }
+
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) onlyOwner public {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
+
+contract Attestation is Ownable {
 
     enum Direction {Forwards, Backwards}
 
@@ -29,13 +65,13 @@ contract Attestation {
 
     mapping (address => mapping(address => uint)) CompanyUsers;
 
-    modifier onlyCompanyOwner(address _company_address) {
-        require(CompanyUsers[_company_address][msg.sender] == 1);
+    modifier onlyCompanyOwner(address _company_address, address _company_owner) {
+        require(CompanyUsers[_company_address][_company_owner] == 1);
         _;
     }
 
-    modifier onlyCompanyUser(address _company_address) {
-        require(CompanyUsers[_company_address][msg.sender] != 0);
+    modifier onlyCompanyUser(address _company_address,address _company_user) {
+        require(CompanyUsers[_company_address][_company_user] != 0);
         _;
     }
 
@@ -44,43 +80,43 @@ contract Attestation {
         _;
     }
 
-    function createUser(address _user_address,bytes32 _data) {
+    function createUserByOwner(address _user_address,bytes32 _data) onlyOwner {
         require(!Users[_user_address].active);
         UserDetails storage userDetails = Users[_user_address];
         userDetails.active = true;
         userDetails.data = _data;
     }
 
-    function createCompany(bytes32 _data,address _company_address) {
+    function createCompanyByOwner(bytes32 _data,address _company_address,address _company_owner) onlyOwner {
         require(!Company[_company_address].active);
         /* create company */
         CompanyDetails storage companyDetails = Company[_company_address];
         companyDetails.active = true;
-        companyDetails.owner = msg.sender;
+        companyDetails.owner = _company_owner;
         companyDetails.data = _data;
         /* setting owner */
-        CompanyUsers[_company_address][msg.sender] = 1;
+        CompanyUsers[_company_address][_company_owner] = 1;
     }
 
-    function editCompany(bytes32 _data,address _company_address) companyExist(_company_address) onlyCompanyOwner(_company_address) {
+    function editCompany(bytes32 _data,address _company_address, address _company_owner) companyExist(_company_address) onlyCompanyOwner(_company_address,_company_owner) {
         CompanyDetails storage companyDetails = Company[_company_address];
         companyDetails.active = true;
         companyDetails.owner = msg.sender;
         companyDetails.data = _data;
     }
 
-    function attestByUser(address _company_address, bytes32 _connection_type, Direction _direction, uint256 _start, uint256 _end) companyExist(_company_address){
+    function attestByUserByOwner(address _user_address,address _company_address, bytes32 _connection_type, Direction _direction, uint256 _start, uint256 _end) onlyOwner companyExist(_company_address){
         if(!Users[msg.sender].active)
         {
-            createUser(msg.sender,bytes32(0));
+            createUserByOwner(_user_address,bytes32(0));
         }
         upsertConnection(msg.sender,_company_address,_connection_type,_direction,_start,_end);
     }
 
-    function attestByCompany(address _user_address,address _company_address, bytes32 _connection_type, Direction _direction, uint256 _start, uint256 _end) companyExist(_company_address) onlyCompanyUser(_company_address) {
+    function attestByCompanyByOwner(address _user_address,address _company_address,address _company_user,bytes32 _connection_type, Direction _direction, uint256 _start, uint256 _end) onlyOwner companyExist(_company_address) onlyCompanyUser(_company_address,_company_user) {
         if(!Users[_user_address].active)
         {
-            createUser(_user_address,bytes32(0));
+            createUserByOwner(_user_address,bytes32(0));
         }
         upsertConnection(_user_address,_company_address,_connection_type,_direction,_start,_end);
     }
@@ -94,13 +130,13 @@ contract Attestation {
         connection.direction = _direction;
     }
 
-    function changeCompanyOwner(address _company_address,address _new_owner_address) companyExist(_company_address) onlyCompanyOwner(_company_address) {
+    function changeCompanyOwnerByOwner(address _company_address,address _company_owner,address _new_owner_address) onlyOwner companyExist(_company_address) onlyCompanyOwner(_company_address,_company_owner) {
         Company[_company_address].owner = _new_owner_address;
         delete CompanyUsers[_company_address][msg.sender];
         CompanyUsers[_company_address][_new_owner_address] = 1;
     }
 
-    function addCompanyUser(address _company_address,address _address) companyExist(_company_address) onlyCompanyOwner(_company_address) {
+    function addCompanyUserByOwner(address _company_address,address _company_owner,address _address) onlyOwner companyExist(_company_address) onlyCompanyOwner(_company_address,_company_owner) {
         CompanyUsers[_company_address][_address] = 2;
     }
 
