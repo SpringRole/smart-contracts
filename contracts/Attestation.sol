d35f5f2ae455c04a571986949561fdb01e2b65cb
Vinay Agarwal <vinay035@gmail.com>
2018-06-28 01:10:23 +0530

    updated contract and test fix

--- contracts/Attestation.sol
+++ contracts/Attestation.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.19;
 
 contract Attestation {
 
-    event Attest(string indexed _type,string _data);
+    event Attest(string _type,string _data);
 
     function write(string _type,string _data) public returns (bool) {
         Attest(_type,_data);
