6cf1aca01472e7535241a5309a2d602d981d3768
Vinay Agarwal <vinay035@gmail.com>
2018-06-28 09:58:20 +0530

    adding address to event

--- contracts/Attestation.sol
+++ contracts/Attestation.sol
@@ -2,10 +2,10 @@ pragma solidity ^0.4.19;
 
 contract Attestation {
 
-    event Attest(string _type,string _data);
+    event Attest(address _address,string _type,string _data);
 
     function write(string _type,string _data) public returns (bool) {
-        Attest(_type,_data);
+        Attest(msg.sender,_type,_data);
         return true;
     }
 }
