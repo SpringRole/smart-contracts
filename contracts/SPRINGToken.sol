a4f224c19810fbf3ab7792c8dc49a2a24f5814fa
Vinay Agarwal <vinay035@gmail.com>
2018-11-04 15:27:06 +0530

    moving to solidity 0.4.24

--- contracts/SPRINGToken.sol
+++ contracts/SPRINGToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.24;
 
 
 /**
@@ -51,7 +51,7 @@ contract Ownable {
    */
   function transferOwnership(address newOwner) onlyOwner public {
     require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
+    emit OwnershipTransferred(owner, newOwner);
     owner = newOwner;
   }
 
@@ -89,7 +89,7 @@ contract Pausable is Ownable {
    */
   function pause() onlyOwner whenNotPaused public {
     paused = true;
-    Pause();
+    emit Pause();
   }
 
   /**
@@ -97,7 +97,7 @@ contract Pausable is Ownable {
    */
   function unpause() onlyOwner whenPaused public {
     paused = false;
-    Unpause();
+    emit Unpause();
   }
 }
 
@@ -116,7 +116,7 @@ contract StandardToken is ERC20,Pausable {
         require(balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]);
         balances[msg.sender] = balances[msg.sender].sub(_value);
         balances[_to] = balances[_to].add(_value);
-        Transfer(msg.sender, _to, _value);
+        emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
@@ -131,7 +131,7 @@ contract StandardToken is ERC20,Pausable {
         balances[_to] = balances[_to].add(_value);
         balances[_from] = balances[_from].sub(_value);
         allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-        Transfer(_from, _to, _value);
+        emit Transfer(_from, _to, _value);
         return true;
     }
 
@@ -154,7 +154,7 @@ contract StandardToken is ERC20,Pausable {
     function approve(address _spender, uint256 _value) whenNotPaused returns (bool success) {
         require(allowed[msg.sender][_spender] == 0);
         allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
@@ -174,7 +174,7 @@ contract StandardToken is ERC20,Pausable {
      */
     function increaseApproval(address _spender, uint _addedValue) whenNotPaused public returns (bool) {
         allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
-        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
         return true;
     }
 
@@ -185,7 +185,7 @@ contract StandardToken is ERC20,Pausable {
         } else {
             allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
         }
-        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
         return true;
     }
 }
@@ -229,7 +229,7 @@ contract SPRINGToken is StandardToken {
 
   /* Contructor function to set maxSupply*/
   function SPRINGToken(uint256 _maxSupply){
-    maxSupply = _maxSupply.mul(10**decimals);
+    maxSupply = _maxSupply;
   }
 
   /**
@@ -241,7 +241,7 @@ contract SPRINGToken is StandardToken {
     require (maxSupply >= (totalSupply.add(_amount)));
     totalSupply = totalSupply.add(_amount);
     balances[msg.sender] = balances[msg.sender].add(_amount);
-    Transfer(address(0), msg.sender, _amount);
+    emit Transfer(address(0), msg.sender, _amount);
     return true;
   }
 
