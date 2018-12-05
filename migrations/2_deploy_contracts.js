e143f872da40aff2bcb391892a181fe29071d2e4
karan <karan@blockwala.io>
2018-12-05 21:02:01 +0530

    airdrop changes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,9 +3,10 @@ var AirDrop = artifacts.require("./AirDrop.sol");
 var SpringToken = artifacts.require("./SPRINGToken.sol");
 var Attestation = artifacts.require("./Attestation.sol");
 
-module.exports = function(deployer,network,accounts) {
-    deployer.deploy(SpringToken,1000000).then(function() {
-        return deployer.deploy(AirDrop, SpringToken.address);
+module.exports = function(deployer, network, accounts) {
+    deployer.deploy(SpringToken, 1000000)
+    .then(function() {
+        return deployer.deploy(AirDrop, 0x1d08d05638a2a006707cec99dba40aae3b9d54fc);
     }).then(function() {
         return deployer.deploy(VanityURL);
     }).then(function() {
--- truffle.js
+++ truffle.js
@@ -1,3 +1,5 @@
+var HDWalletProvider = require("truffle-hdwallet-provider-privkey");
+
 module.exports = {
   networks: {
     development: {
@@ -5,6 +7,14 @@ module.exports = {
       port: 8545,
       gas: 6721975,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: function() {
+              return new HDWalletProvider(["87BAA9CC66610DA8466516EFC29A6AFC167113EAEAE907637874A4F9AF01868F"],
+                    "https://ropsten.infura.io/BbBlRVGSUuAagrvyIjdr")
+            },
+      network_id: 3,
+      gas: 4600000
     }
   }
 };
